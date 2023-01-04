d1196e101ac3e0d5cd81ed882e7d7bfabb7645ff
Lucas dos Santos Silva <110935148+olaLucas@users.noreply.github.com>
2023-01-03 21:13:32 -0300

    Update sorting algoritm.c
    some parts was changed after I realize an better way to write the code.

--- sorting algoritm.c
+++ sorting algoritm.c
@@ -7,29 +7,28 @@ void swap(int *x, int *y)
     *y = swap;
 }
 
-
-void sort(int arrSize, int array[])
+void sort(int array[], int arraySize)
 {
     int min_index = 0;
 
-    for (int i = 0; i < arrSize - 1; i++)
+    for (int i = 0; i < arraySize - 1; i++)
     {
-        for (int j = i + 1; j < arrSize; j++)
+        for (int j = i + 1; j < arraySize; j++)
         {
-            min_index = i;
-            if (array[j] < array[min_index])
-            {  
-                min_index = j;
-
-                if (min_index != i)
+            if (array[j] < array[i])
+            {   
+                if (j != i)
                 {
-                    swap(&array[min_index], &array[i]);
-                }
+                    swap(&array[j], &array[i]);
+                }   
             }
         }
     }
+}
 
-    for (int i = 0; i < arrSize; i++)
+void printing(int array[], int arraySize)
+{
+    for (int i = 0; i < arraySize; i++)
     {
         printf("%d ", array[i]);
     }
@@ -39,10 +38,11 @@ void sort(int arrSize, int array[])
 
 int main(void)
 {
-    int array[10] = {5,7,3,2,8,6,4,1,9,10};
-    int arrSize = sizeof(array)/sizeof(array[0]);
+    int array[10] = {9,3,2,6,8,10,4,1,7,5};
+    int arraySize = sizeof(array)/sizeof(array[0]);
 
-    sort(arrSize, array);
-   
+    sort(array, arraySize);
+    printing(array, arraySize);
+
     return 0;
 }
